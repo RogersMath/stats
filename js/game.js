5094dc33cff981186f8f7c862b4b0ce7d0efc2e1
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-09-20 13:59:03 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ let sessionBest = {
     incorrect: 0,
     percentage: 0
 };
-let currentLibrary = 'Functions';
+let currentLibrary = 'ch1_and_ch2_terms';
 let currentQuestions = [];
 let answeredQuestions = [];
 
